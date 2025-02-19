1b43fbeb10b37f811558dd73b8e609232274c634
Wizxrdx <benedick.dumol@gmail.com>
2025-02-19 17:35:54 +0800

    removed: close button on sidebar

--- app/static/dashboard/main.js
+++ app/static/dashboard/main.js
@@ -25,22 +25,12 @@ function updateTable() {
 
 document.addEventListener("DOMContentLoaded", function () {
     const navToggle = document.querySelector(".nav-toggle");
-    const navClose = document.querySelector(".nav-close");
     const navMenu = document.querySelector("nav ul");
 
     navToggle.addEventListener("click", function () {
         navMenu.classList.toggle("show");
     });
-    navClose.addEventListener("click", function () {
-        navMenu.classList.toggle("show");
     });
-});
-
-function replaceWithContent(selector, url) {
-    $.get(url, function(data) {
-        $(selector).replaceWith(data);
-    });
-}
 
 $(document).ready(function() {
     replaceWithContent("#status", "/status");
