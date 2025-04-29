397a854541ef9c665c80c63b983f00a5b90ccf64
Wizxrdx <benedick.dumol@gmail.com>
2025-04-29 17:50:58 +0800

    added machine directory to modules

--- machine/comms.py
+++ machine/comms.py
@@ -1,6 +1,6 @@
 import requests
-from main_button import Machine
-import servo_and_loadcell_1kg as system_1kg
+from machine.main_button import Machine
+import machine.servo_and_loadcell_1kg as system_1kg
 
 def start_1kg():
     Machine().change_system(system_1kg)
@@ -8,9 +8,6 @@ def start_1kg():
 def start_10kg():
     # Machine().change_system("10kg")
     pass
-
-def finish_1kg():
-    requests.put("http://127.0.0.1:5000/count/1kg")
 
 def finish_10kg():
     requests.put("http://127.0.0.1:5000/count/10kg")
