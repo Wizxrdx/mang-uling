f28d6f0529a5c914d0710ed881316318e0b9b808
Wizxrdx <benedick.dumol@gmail.com>
2025-05-02 01:30:43 +0800

    added notification request

--- app/static/dashboard/notification.js
+++ app/static/dashboard/notification.js
@@ -1,15 +1,22 @@
 function toggleNotification() {
     if (Notification.permission === 'default') {
       Notification.requestPermission().then(permission => {
-        if (permission === 'granted') {
-          document.getElementById('notifyIcon').textContent = 'notifications_active';
-          new Notification("Notifications enabled!");
-        }
-      });
+            if (permission === 'granted') {
+            document.getElementById('notifyIcon').textContent = 'notifications_active';
+            new Notification("Notifications enabled!");
+            }
+        });
     } else if (Notification.permission === 'granted') {
-      document.getElementById('notifyIcon').textContent = 'notifications_active';
-      new Notification("Notifications already enabled!");
+        document.getElementById('notifyIcon').textContent = 'notifications_active';
+        new Notification("Notifications already enabled!");
     } else {
-      alert("Notifications are blocked.");
+        alert("Notifications are blocked.");
     }
   }
+ 
+Notification.requestPermission();
+const source = new EventSource("/notify");
+
+source.onmessage = function(event) {
+    new Notification("Packing Done", { body: event.data });
+};
