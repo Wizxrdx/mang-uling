65f9d331b5d2fc6b5d74d577eebf3ef876b58bc1
Wizxrdx <benedick.dumol@gmail.com>
2025-05-22 00:27:47 +0800

    fixed pins not allocated

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 import os
 from app import create_app
-from machine.main_button import Machine
 
 os.system('sudo ntpdate time.google.com')
 app = create_app()
 
 if __name__ == "__main__":
-    Machine()
     app.run(debug=False, host='0.0.0.0', port=5000)
